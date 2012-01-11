7834fe284a55b2c2f771d75dc7df5b8df035e574
Juan Lopes <me@juanlopes.net>
2012-01-11 01:14:44 -0200

    rustic install script

---
+++ src/shell/stub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# simple script for turning a jar with a Main-Class
+# into a stand alone executable
+# cat [your jar file] >> [this file]
+# then chmod +x [this file]
+# you can now exec [this file]
+exec java -Xmx1G -jar "$0" $@
