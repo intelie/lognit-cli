1ea2641a1e6e9d730f05aef37b993220a58dc07b
Juan Lopes <me@juanlopes.net>
2012-04-05 17:06:57 -0300

    better install

---
+++ tools/api/log-groups.sh
@@ -0,0 +1,3 @@
+cat << '__END__' | nitr post log-groups -d @-
+{"name":"3809ad5b-c401-4c2b-b08a-5cdeec97e9f2","spaces":[],"metadata":[{"key":"d","value":"e"}],"patterns":[{"expression":"c"}],"templates":[{"position":123,"property":"f"}],"filters":[{"id":"ff80818136736fbf01367370f6d70002","expressions":[{"id":"ff80818136736fbf01367370f6d80003","key":"a","value":"b"}]}]}
+__END__
