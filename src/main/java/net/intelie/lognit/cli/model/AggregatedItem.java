ff7a2be10c9a1e65d4b2ce9b19ac3a06eca28d77
Juan Lopes <me@juanlopes.net>
2012-05-30 17:52:08 -0300

    now really solving double formatting issues

--- src/main/java/net/intelie/lognit/cli/model/AggregatedItem.java
+++ src/main/java/net/intelie/lognit/cli/model/AggregatedItem.java
@@ -3,15 +3,15 @@ package net.intelie.lognit.cli.model;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
+import java.text.DecimalFormat;
 import java.util.LinkedHashMap;
 
 public class AggregatedItem extends LinkedHashMap<String, Object> {
-    private static final MathContext MATH = MathContext.DECIMAL64;
 
     @Override
     public Object put(String s, Object o) {
         if (o instanceof Double)
-            o = new BigDecimal((Double)o, MATH).stripTrailingZeros();
+            o = new BigDecimal(new DecimalFormat("#.######").format(o));
         return super.put(s, o);
     }
 }
