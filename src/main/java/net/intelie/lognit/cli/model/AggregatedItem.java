aa6555ba2f793944a27881d2f8533587fc4a9765
Juan Lopes <me@juanlopes.net>
2012-09-10 11:46:56 -0300

    fixing aggregated item with current locale

--- src/main/java/net/intelie/lognit/cli/model/AggregatedItem.java
+++ src/main/java/net/intelie/lognit/cli/model/AggregatedItem.java
@@ -4,14 +4,17 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
 import java.util.LinkedHashMap;
+import java.util.Locale;
 
 public class AggregatedItem extends LinkedHashMap<String, Object> {
+    public static final DecimalFormatSymbols SYMBOLS = new DecimalFormatSymbols(Locale.ENGLISH);
 
     @Override
     public Object put(String s, Object o) {
         if (o instanceof Double)
-            o = new BigDecimal(new DecimalFormat("#.######").format(o));
+            o = new BigDecimal(new DecimalFormat("#.######", SYMBOLS).format(o));
         return super.put(s, o);
     }
 }
