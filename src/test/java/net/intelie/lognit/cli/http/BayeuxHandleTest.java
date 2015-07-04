09e16845bf2ea5366397706b3dfbf89b5cae83e8
Juan Lopes <me@juanlopes.net>
2015-07-04 02:53:02 -0300

    release version 1.7.3

--- src/test/java/net/intelie/lognit/cli/http/BayeuxHandleTest.java
+++ src/test/java/net/intelie/lognit/cli/http/BayeuxHandleTest.java
@@ -8,16 +8,17 @@ import static org.mockito.Mockito.*;
 public class BayeuxHandleTest {
     @Test
     public void willDisconnectWhenClosing() {
-        BayeuxClient client = mock(BayeuxClient.class);
-        BayeuxHandle handle = new BayeuxHandle(client, channel);
+        BayeuxClient client = mock(BayeuxClient.class, RETURNS_DEEP_STUBS);
+        BayeuxHandle handle = new BayeuxHandle(client, "abc");
         handle.close();
+        verify(client.getChannel("abc")).unsubscribe();
         verify(client).disconnect();
     }
 
     @Test
     public void waitDisconnectedWaitsForSoLong() {
         BayeuxClient client = mock(BayeuxClient.class);
-        BayeuxHandle handle = new BayeuxHandle(client, channel);
+        BayeuxHandle handle = new BayeuxHandle(client, "abc");
         when(client.waitFor(1000, BayeuxClient.State.UNCONNECTED, BayeuxClient.State.DISCONNECTED)).thenReturn(true);
         handle.waitDisconnected();
         verify(client).waitFor(1000, BayeuxClient.State.UNCONNECTED, BayeuxClient.State.DISCONNECTED);
@@ -26,10 +27,12 @@ public class BayeuxHandleTest {
 
     @Test
     public void onlyClosesOnce() {
-        BayeuxClient client = mock(BayeuxClient.class);
-        BayeuxHandle handle = new BayeuxHandle(client, channel);
+        BayeuxClient client = mock(BayeuxClient.class, RETURNS_DEEP_STUBS);
+        BayeuxHandle handle = new BayeuxHandle(client, "abc");
         handle.close();
+        verify(client.getChannel("abc")).unsubscribe();
         verify(client).disconnect();
+        reset(client);
         handle.close();
         verifyNoMoreInteractions(client);
     }
